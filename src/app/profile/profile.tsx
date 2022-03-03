5ec938f8796041316d4ddd1a85e9976314874338
taras-d <taras.datsenko@gmail.com>
2022-03-03 11:41:13 +0200

    Profile form

--- src/app/profile/profile.tsx
+++ src/app/profile/profile.tsx
@@ -1,9 +1,31 @@
+import { Input } from '@shared/ui';
+import { currentUser } from '@shared/utils';
+
 import './profile.less';
 
 export function Profile() {
+  const user = currentUser.user;
+
   return (
     <div className="profile">
-      Profile
+      <div className="row">
+        <div className="col-3">Email</div>
+        <div className="col-9">
+          <Input defaultValue={user.email} disabled />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-3">Created</div>
+        <div className="col-9">
+          <Input defaultValue={user.createdAt} disabled />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-3">Updated</div>
+        <div className="col-9">
+          <Input defaultValue={user.updatedAt} disabled />
+        </div>
+      </div>
     </div>
   );
 }
--- src/shared/ui/input/input.tsx
+++ src/shared/ui/input/input.tsx
@@ -3,19 +3,23 @@ import './input.less';
 interface InputProps {
   name?: string;
   value?: string;
+  defaultValue?: string;
   type?: string;
   placeholder?: string;
+  disabled?: boolean;
   onChange?: React.ChangeEventHandler<HTMLInputElement>;
 }
 
-export function Input({ name, value, type, placeholder, onChange }: InputProps) {
+export function Input({ name, value, defaultValue, type, placeholder, disabled, onChange }: InputProps) {
   return (
     <input
       className="input"
       value={value}
+      defaultValue={defaultValue}
       type={type}
       name={name}
       placeholder={placeholder}
+      disabled={disabled}
       onChange={onChange}
     />
   );
