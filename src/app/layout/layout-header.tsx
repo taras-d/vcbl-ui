19c9ad921bff2166ea2a9aa33e661a8129efb3f4
taras-d <taras.datsenko@gmail.com>
2022-03-21 22:51:43 +0200

    Header translation

--- src/app/layout/layout-header.tsx
+++ src/app/layout/layout-header.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
-import { history, events } from '@shared/utils';
+import { history, events, tkey } from '@shared/utils';
 import { AppRoutes, EventTypes } from '@shared/interfaces';
 import { Select } from '@shared/ui';
 import './layout-header.less';
@@ -19,8 +19,8 @@ export function LayoutHeader() {
   return (
     <div className="layout-header">
       <Select value={value} onChange={handleValueChange}>
-        <option value={AppRoutes.Home}>Vocabulary</option>
-        <option value={AppRoutes.Profile}>Profile</option>
+        <option value={AppRoutes.Home}>{tkey('header_vocabulary')}</option>
+        <option value={AppRoutes.Profile}>{tkey('header_profile')}</option>
       </Select>
     </div>
   );
