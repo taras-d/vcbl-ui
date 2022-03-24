e9a18037478f2cdbdb2768942926610dc11682c7
taras-d <taras.datsenko@gmail.com>
2022-03-24 13:07:29 +0200

    Add variables file

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,13 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      less: {
+        paths: ['./src/styles']
+      }
+    }
+  },
   plugins: [
     react(),
     tsconfigPaths()
